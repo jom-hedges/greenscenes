a4d24218d9db68173c7bd9c2dec7a0a93897ca6e
jom-hedges <jom_hedges@proton.me>
2025-05-14 12:01:59 +0800

    push to date

--- config/config.exs
+++ config/config.exs
@@ -61,6 +61,17 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
+# Configures AWS and S3
+config :ex_aws,
+  access_key_id: [{:system, "AWS_ACCESS_KEY_ID"}, :instance_role],
+  secret_access_key: [{:system, "AWS_SECRET_ACCESS_KEY"}, :instance_role],
+  region: "us-east-2"
+
+config :ex_aws, s3,
+  scheme: "https://",
+  host: "s3.amazonaws.com",
+  region: "us-east-2"
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
